1cca2d2eca8bebd2f73855c864eb6db76597ded0
Naveen Danu <naveendanu2000@gmail.com>
2026-02-13 17:16:20 +0530

    Added the routes for Authors Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BooksSection from "./components/BooksSection";
+import AuthorsSection from "./components/AuthorsSection";
 
 const App = () => {
   return (
     <div className="overflow-x-hidden">
-      <BooksSection />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BooksSection />} />
+          <Route path="/authors/:id" element={<AuthorsSection />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/components/AuthorsSection.tsx
+++ src/components/AuthorsSection.tsx
@@ -1,6 +1,8 @@
+import { useParams } from "react-router-dom";
 import SearchSection from "./SearchSection";
 
-const AuthorsSection = ({ id }: { id: number }) => {
+const AuthorsSection = () => {
+  const { id } = useParams();
   return (
     <SearchSection
       searchApi={`/api/books/search/authorid/${id}`}
